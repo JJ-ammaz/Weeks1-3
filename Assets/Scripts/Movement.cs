178acfc93b6a55564ac79a8e49d75a8ed94414ad
JJ-ammaz <julian.jjammaz@gmail.com>
2026-01-20 17:43:03 -0500

    so far

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,51 +1,61 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     public float speed = 5f;
     public bool Flipped = true;
+    Vector2 bottomleft;
+    Vector2 topright;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        bottomleft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        topright = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 
     // Update is called once per frame
     void Update()
     {
         //rotation
-        Vector3 newRotation = transform.eulerAngles;
-        transform.eulerAngles = newRotation;
+ 
 
 
         //movement
         Vector3 newPosition = transform.position;
         newPosition.x += speed * Time.deltaTime;
-        transform.position = newPosition;
 
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
 
         //movement if's
-        if (screenPos.x < 0)
+        if (screenPos.x < 10)
         {
+            newPosition.x = bottomleft.x;
             speed = speed * -1;
             Flipped = true;
         }
 
         if (screenPos.x > Screen.width)
         {
+            newPosition.x = topright.x;
             speed = speed * -1;
-            Flipped = false;
+            Flipped = true;
         }
 
         //rotation/flips if's
 
         if (Flipped == true)
         {
+            Flipped = false;
+            Vector3 newRotation = transform.eulerAngles;
+            newRotation.z += 180;
+            newRotation.x += 180;
+            transform.eulerAngles = newRotation;
             print("hello world");
         }
 
+        transform.position = newPosition;
 
     }
 }
